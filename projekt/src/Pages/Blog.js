2f4686f2f260068a7b1145a7098cef285268a0fa
Jelena Runje <jelena_bukovac@hotmail.com>
2026-01-23 17:42:04 +0100

    dodane_promjene

--- projekt/src/Pages/Blog.js
+++ projekt/src/Pages/Blog.js
@@ -1,41 +1,27 @@
-
-import posts from "../components/zadaci/data/blog.json";
-import posts from "./Blog.css";
-
-
+import './Blog.css';
+import posts from '../components/zadaci/data/blog.json';
 
 const Blog = () => {
-    return(
-        <>
-      <div className="blog-page">
-
+  return (
+    <div className="blog-page">
         <div className="container">
-           
             <h1>Blog</h1>
-            <div className = "row">
-            {posts.map((post) =>(
-                <div className="blog-post col-md-4 mb-4">
-                    <img src={post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} className="mb-3" alt={post.title.rendered} />
-                   <h2>{post.title.rendered}</h2>
-                    <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></div>
-                  <p>{post._embedded.author[0].name} | {new Date(post.date).toLocaleString("hr-HR", {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
+            <div className="row">
+              { posts.map((post) => (
+                <div className="col-md-4 mb-4 blog-post">
+                  <img src={post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} className="mb-3" alt={post.title.rendered} />
+                  <h2>{post.title.rendered}</h2>
+                  <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></div>
+                  <p>{post._embedded.author[0].name} | {new Date(post.date).toLocaleDateString("hr-HR", {
                     hour: '2-digit',
                     minute: '2-digit',
                   })}</p>
-                 
-                    </div>
-            ))
-}
+                </div>
+              )) }
             </div>
-
-        
-            </div>
-
-         </div>
-</>
-    );
+        </div>
+    </div>
+  );
 };
+
 export default Blog;
