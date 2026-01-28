7284ffbd45b3793b74fcea30b02a257834a33ca5
Jelena Runje <jelena_bukovac@hotmail.com>
2026-01-28 19:46:01 +0100

    novo

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -1,43 +1,32 @@
+
 import { BrowserRouter, Routes, Route } from "react-router";
-
+import './Gutenberg.css';
 import './App.css';
-
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Naslovnica from './pages/Naslovnica';
-import Blog from './pages/Blog';
-import BlogSingle from './pages/BlogSingle';
-import Onama from "./pages/Onama";
-import Usluge from "./pages/Usluge";
-import Kontakt from "./pages/Kontakt";
-
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from './Vozila.js/Home';
+import Blog from './Vozila.js/Blog';
+import BlogSingle from "./Vozila.js/Blogsingle";
 import Profil from "./components/zadaci/Profil";
-import Korisnici from "./components/zadaci/Korisnici";
-import Tecaj from "./components/zadaci/Tecaj";
-
+import Korisnici from "./components/zadaci/Korisnici"
+import Tecaj from "./components/zadaci/Tecaj"
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
+      <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Naslovnica />} />
+        <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blogsingle" element={<BlogSingle />} />
-        <Route path="/o-nama" element={<Onama />} />
-        <Route path="/usluge" element={<Usluge />} />
-        <Route path="/kontakt" element={<Kontakt />} />
-
-
         <Route path="/profil" element={<Profil />} />
-        <Route path="/korisnici" element={<Korisnici />} />
-        <Route path="/tecaj" element={<Tecaj />} />
-
-
-
+        <Route path="/korisnici"element={<Korisnici />} /> 
+        <Route path="/tecaj"element={<Tecaj />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   );
 }
 
